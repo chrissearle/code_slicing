ca00aa1b0c7cc44cf9b00606171333c3ef4fad5d
Chris Searle <chris@chrissearle.org>
2021-10-02 18:26:03 +0200

    Initial Commit

---
+++ Scratch/ScratchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchApp.swift
+//  Scratch
+//
+//  Created by Chris Searle on 02/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScratchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
